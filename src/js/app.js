774d2c19b2f84f383f55bf58ff870e2f4c0e43ed
brandonT2002 <brandonteja02@gmail.com>
2024-07-22 21:47:32 -0600

    [update] dark mode system

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,23 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function darkMode() {
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    // console.log(prefiereDarkMode)
+
+    if (prefiereDarkMode.matches === true) {
+        document.body.classList.toggle('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if (prefiereDarkMode.matches === true) {
+            document.body.classList.toggle('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    })
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
     botonDarkMode.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
