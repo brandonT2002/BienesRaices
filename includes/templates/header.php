4bb4c238f0f2bed265fefb0ccc29ec96c2769c48
brandonT2002 <brandonteja02@gmail.com>
2024-07-23 19:58:24 -0600

    [update] estructura

---
+++ includes/templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="/build/css/app.css">
+</head>
+<body>
+    <header class="header <?php echo $inicio ? 'inicio' : ''; ?>">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="/">
+                    <img src="build/img/logo.svg" alt="logo bienes raices">
+                </a>
+                <div class="mobile-menu">
+                    <img class="menuIcono" src="build/img/barras.svg" alt="icono menu responsive">
+                </div>
+                <div class="derecha">
+                    <img src="build/img/dark-mode.svg" alt="dark mode" class="dark-mode-boton">
+                    <nav class="navegacion">
+                        <a href="nosotros.php">Nosotros</a>
+                        <a href="anuncios.php">Anuncios</a>
+                        <a href="blog.php">Blog</a>
+                        <a href="contacto.php">Contacto</a>
+                    </nav>
+                </div>
+            </div>
+            <?php echo $inicio ? '<h1>Venta de Casas y Departamentos Exclusivos</h1>' : ''; ?>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -1,35 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-    <header class="header inicio">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="/">
-                    <img src="build/img/logo.svg" alt="logo bienes raices">
-                </a>
-                <div class="mobile-menu">
-                    <img class="menuIcono" src="build/img/barras.svg" alt="icono menu responsive">
-                </div>
-                <div class="derecha">
-                    <img src="build/img/dark-mode.svg" alt="dark mode" class="dark-mode-boton">
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-            </div>
-            <h1>Venta de Casas y Departamentos Exclusivos</h1>
-        </div>
-    </header>
-
+<?php
+    $inicio = true;
+    include './includes/templates/header.php'
+?>
     <main class="contenedor seccion">
         <h1>Más Sobre Nosotros</h1>
 
@@ -212,18 +184,6 @@
         </section>
     </div>
 
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.html">Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contacto.html">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los derechos reservados 2024 &copy</p>
-    </footer>
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php
+    include 'includes/templates/footer.php'
+?>
