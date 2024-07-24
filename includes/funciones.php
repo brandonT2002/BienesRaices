9f26ab0e37f75bacbd08a61d1d7937cdf8281828
brandonT2002 <brandonteja02@gmail.com>
2024-07-23 20:20:48 -0600

    [update] funciones

---
+++ includes/funciones.php
@@ -0,0 +1,8 @@
+<?php
+require 'app.php';
+
+function incluirTemplate( String $nombre, bool $inicio = false ) {
+    include TEMPLATES_URL . "/${nombre}.php";
+}
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    $inicio = true;
-    include './includes/templates/header.php'
+    require 'includes/funciones.php';
+    incluirTemplate('header', $inicio = true);
 ?>
     <main class="contenedor seccion">
         <h1>Más Sobre Nosotros</h1>
@@ -185,5 +185,5 @@
     </div>
 
 <?php
-    include 'includes/templates/footer.php'
+    incluirTemplate('footer');
 ?>
